044d8874a1b54b557246299a4935b45f88a19480
Davi Moreira Fuzatto <davimoreiraf@gmail.com>
2023-02-20 19:14:30 -0300

    primeiro commit

---
+++ src/main/java/com/peopleManagement/davi/DaviApplication.java
@@ -0,0 +1,13 @@
+package com.peopleManagement.davi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peopleManagement/davi/DaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peopleManagement.davi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
